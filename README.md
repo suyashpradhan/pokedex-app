f2c373155f858dd4894818d5b61246e12e6fe18d
suyashpradhan <pradhansuyash39@gmail.com>
2022-10-10 23:45:58 +0530

    feat: init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- @format -->
+
+Styled Components with Styled System
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Styled Components</h1>
+		</div>
+	);
+}
+
+export default App;
