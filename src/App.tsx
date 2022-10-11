b2f352c768e3799e24e921c19bbedfc608625b5b
suyashpradhan <pradhansuyash39@gmail.com>
2022-10-11 23:44:15 +0530

    feat: adds basic get api for fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 /** @format */
 
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+	const { data, isLoading } = useQuery(['pokemon'], () =>
+		fetch('https://pokeapi.co/api/v2/pokemon/?limit=20').then((res) => res.json()),
+	);
+
+	console.log(data);
+
 	return (
 		<div className='App'>
-			<h1>Styled Components</h1>
+			{isLoading ? <p>loading...</p> : data?.results?.map((d: any) => <h4>{d?.name}</h4>)}
 		</div>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 /** @format */
 
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const client = new QueryClient();
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={client}>
+			<ReactQueryDevtools position='bottom-left' />
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
